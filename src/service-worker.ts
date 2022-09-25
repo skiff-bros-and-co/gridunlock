93501c39527b6b434e6a95a5ef7b2a38ae269286
David Skiff <dskiff@users.noreply.github.com>
2022-09-24 22:42:30 -0500

    green

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,21 +1,21 @@
-const cacheName = "assets";
-const filesToCache = ["/", "/index.html", "/js/index.css", "/js/index.js"];
+// const cacheName = "assets";
+// const filesToCache = ["/", "/index.html", "/js/index.css", "/js/index.js"];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener("install", function (e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
-      return cache.addAll(filesToCache);
-    }),
-  );
-  self.skipWaiting();
-});
+// self.addEventListener("install", function (e) {
+//   e.waitUntil(
+//     caches.open(cacheName).then(function (cache) {
+//       return cache.addAll(filesToCache);
+//     }),
+//   );
+//   self.skipWaiting();
+// });
 
-/* Serve cached content when offline */
-self.addEventListener("fetch", function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
-    }),
-  );
-});
+// /* Serve cached content when offline */
+// self.addEventListener("fetch", function (e) {
+//   e.respondWith(
+//     caches.match(e.request).then(function (response) {
+//       return response || fetch(e.request);
+//     }),
+//   );
+// });
