920a98399722e5313b7ae0ea7bc482fb8dcff8c3
Ben Skiff <benskiff@msn.com>
2022-10-02 13:16:03 -0500

    Allow dynamic columns/rows using CSS variables

--- src/components/PuzzleView.tsx
+++ src/components/PuzzleView.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import type { PuzzleGameCell } from "../state/State";
 import { PuzzleCell } from "./PuzzleCell";
 
@@ -5,7 +6,13 @@ interface Props {
   filledCellsByRow: PuzzleGameCell[][];
 }
 
+const setColumnCount = (columnCount: number) => {
+  document.documentElement.style.setProperty("--grid-column-count", `${columnCount}`);
+};
+
 export const PuzzleView = (props: Props): JSX.Element => {
+  useEffect(() => setColumnCount(props.filledCellsByRow[0]?.length), [props.filledCellsByRow[0]?.length]);
+
   const cells: JSX.Element[] = props.filledCellsByRow.flatMap((row) =>
     row.map((cell) => {
       return <PuzzleCell cell={cell} />;
--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -6,16 +6,25 @@ const testPuzzleData: PuzzleGameCell[][] = [
     { author: null, filledValue: "T" },
     { author: null, filledValue: "O" },
     { author: null, filledValue: "" },
+    { author: null, filledValue: "Z" },
   ],
   [
     { author: null, filledValue: "" },
     { author: null, filledValue: "N" },
     { author: null, filledValue: "O" },
+    { author: null, filledValue: "Z" },
   ],
   [
     { author: null, filledValue: "" },
     { author: null, filledValue: "" },
     { author: null, filledValue: "F" },
+    { author: null, filledValue: "Z" },
+  ],
+  [
+    { author: null, filledValue: "" },
+    { author: null, filledValue: "" },
+    { author: null, filledValue: "F" },
+    { author: null, filledValue: "Z" },
   ],
 ];
 export function Root() {
