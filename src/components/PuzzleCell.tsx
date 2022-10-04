b41f85d0be993b4104a6633258b4ffc1b685ea24
Ben Skiff <benskiff@msn.com>
2022-10-03 19:48:29 -0500

    make selected cell pop

--- src/components/PuzzleCell.tsx
+++ src/components/PuzzleCell.tsx
@@ -41,11 +41,15 @@ export const PuzzleCell = (props: Props): JSX.Element => {
     return <div className="grid-cell" />;
   }
 
+  const inputClassFilled = `grid-cell-${props.gameCell.filledValue === "" ? "empty" : "filled"}`;
+  const inputClassSelected = props.isSelected ? "grid-cell-selected" : "";
+  const hintClassSelected = props.isSelected ? "grid-cell-hint-number-selected" : "";
+
   return (
     <div className="grid-cell-wrapper">
       <input
         ref={inputRef}
-        className={`grid-cell grid-cell-${props.gameCell.filledValue === "" ? "empty" : "filled"}`}
+        className={`grid-cell ${inputClassFilled} ${inputClassSelected}`}
         onFocus={props.onSelectCell}
         onChange={(event) => {
           const validInput = getValidInput(event.target.value);
@@ -56,7 +60,7 @@ export const PuzzleCell = (props: Props): JSX.Element => {
         }}
         value={props.gameCell.filledValue}
       />
-      <p className="grid-cell-hint-number">{props.gameCell.clueNumber}</p>
+      <p className={`grid-cell-hint-number ${hintClassSelected}`}>{props.gameCell.clueNumber}</p>
     </div>
   );
 };
