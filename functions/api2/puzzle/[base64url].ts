982ae0d453749cb6fc127f461153f2001009398f
David Skiff <dskiff@palantir.com>
2022-10-05 16:34:47 -0500

    catch errors

--- functions/api2/puzzle/[base64url].ts
+++ functions/api2/puzzle/[base64url].ts
@@ -1,18 +1,20 @@
 import { parsePuz } from "../../../src/parsers/parsePuz";
 
 export const onRequestGet: PagesFunction = async ({ request, params }) => {
-  const base64url = params.base64url as string;
-  const url = atob(base64url);
+  try {
+    const base64url = params.base64url as string;
+    const url = atob(base64url);
 
-  console.info("Got request for", url);
+    console.info("Got request for", url);
 
-  const puzzleData = await getPuzzle(url);
-  const puzzle = await parsePuz(puzzleData);
-  const response = new Response(JSON.stringify(puzzle));
+    const puzzleData = await getPuzzle(url);
+    const puzzle = await parsePuz(puzzleData);
+    const response = new Response(JSON.stringify(puzzle));
 
-  caches.default.put(request, response);
-
-  return response;
+    return response;
+  } catch (e) {
+    return new Response(e?.stack ?? e, { status: 500 });
+  }
 };
 
 async function getPuzzle(url: string) {
