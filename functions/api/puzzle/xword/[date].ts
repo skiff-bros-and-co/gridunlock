3af8997986220baa2783729f5dca276934689465
David Skiff <dskiff@palantir.com>
2022-10-06 13:19:45 -0500

    starting the work to map

--- functions/api/puzzle/xword/[date].ts
+++ functions/api/puzzle/xword/[date].ts
@@ -1,5 +1,11 @@
 import { PuzzleDefinition } from "../../../../src/state/Puzzle";
 
+const TIMEOUTS_SEC = {
+  UNAVAILABLE_KV: 1 * 60 * 60,
+  UNAVAILABLE_CACHE_HEADER: 1 * 60 * 60,
+  AVAILABLE_CACHE_HEADER: 4 * 60 * 60,
+};
+
 interface Env {
   // Defined in the Cloudflare Pages config
   XWORDS: KVNamespace;
@@ -42,26 +48,46 @@ interface XWordInfoJsonFormat {
   notepad: string;
 }
 
-export const onRequestGet: PagesFunction<Env> = async ({ params, env }) => {
+export const onRequestGet: PagesFunction<Env> = async ({ params, env, request }) => {
+  const date = params.date as string;
+  console.info("Got request for", date);
+
   try {
-    const date = params.date as string;
-    console.info("Got request for", date);
+    const puzzle = await fetchPuzzle(date, env);
+    const available: PuzzleCacheEntry = { available: true, puzzle };
+    env.XWORDS.put(date, JSON.stringify(available));
 
-    try {
-      const puzzle = fetchPuzzle(date);
-    } catch (e) {
-      console.error("failed to fetch puzzle", e?.stack ?? e);
-      return new Response("Failed to retrieve puzzle", { status: 500 });
-    }
-
-    return new Response(body);
+    return new Response(JSON.stringify(puzzle), {
+      headers: {
+        "cache-control": `public, max-age=${TIMEOUTS_SEC.AVAILABLE_CACHE_HEADER}`,
+      },
+    });
   } catch (e) {
-    // There's no logging for Cloudflare Functions, yet :(
-    return new Response(e?.stack ?? JSON.stringify(e), { status: 500 });
+    console.error("failed to fetch puzzle", e?.stack ?? e);
+    const unavailable: PuzzleCacheEntry = { available: false };
+    env.XWORDS.put(date, JSON.stringify(unavailable), {
+      expirationTtl: TIMEOUTS_SEC.UNAVAILABLE_KV,
+    });
+
+    return new Response("Failed to retrieve puzzle", {
+      status: 500,
+      headers: {
+        "cache-control": `public, max-age=${TIMEOUTS_SEC.UNAVAILABLE_CACHE_HEADER}`,
+      },
+    });
   }
 };
 
-export async function fetchPuzzle(date: string) {
+export async function fetchPuzzle(date: string, env: Env) {
+  const existing = await env.XWORDS.get<PuzzleCacheEntry>(date, "json");
+  if (existing != null) {
+    if (!existing.available) {
+      throw new Error("puzzle still isn't available");
+    }
+
+    return existing.puzzle;
+  }
+
   const req = await fetch(`https://www.xwordinfo.com/JSON/Data.ashx?format=text&date=${date.replace("-", "/")}`, {
     headers: {
       referer: "https://www.xwordinfo.com/JSON/Sample2",
@@ -80,5 +106,20 @@ export async function fetchPuzzle(date: string) {
     throw new Error("puzzle isn't yet available");
   }
 
-  return parsed;
+  const puzzle: PuzzleDefinition = {
+    title: parsed.title,
+    author: parsed.author,
+    copyright: parsed.copyright,
+    description: parsed.notepad,
+    height: parsed.size.rows,
+    width: parsed.size.cols,
+    cells: [],
+    clues: {
+      across: [],
+      byRowAndColumn: [],
+      clueCount: 0,
+      down: [],
+    },
+  };
+  return puzzle;
 }
