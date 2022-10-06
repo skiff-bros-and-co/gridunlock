18957dbf8f06258ebccb79956629a1205b29fbed
David Skiff <dskiff@palantir.com>
2022-10-05 21:30:19 -0500

    test

--- functions/api/puzzle/xword/[date].ts
+++ functions/api/puzzle/xword/[date].ts
@@ -14,17 +14,14 @@ export const onRequestGet: PagesFunction<Env> = async ({ params, env }) => {
     const date = params.date as string;
     console.info("Got request for", date);
 
-    const req = await fetch(
-      "https://www.xwordinfo.com/JSON/Data.ashx?callback=jQuery3610038867805517661_1665020930216&date=current&_=1665020930217",
-      {
-        headers: {
-          referer: "https://www.xwordinfo.com/JSON/Sample2",
-        },
-        cf: {
-          scrapeShield: false,
-        },
+    const req = await fetch(`https://www.xwordinfo.com/JSON/Data.ashx?format=text&date=${date.replace("-", "/")}`, {
+      headers: {
+        referer: "https://www.xwordinfo.com/JSON/Sample2",
       },
-    );
+      cf: {
+        scrapeShield: false,
+      },
+    });
 
     const body = await req.text();
 
