afe2460050bf23e4b2f17b8703c9b5be42e14bd9
David Skiff <dskiff@palantir.com>
2022-10-06 18:29:57 -0500

    adds a "new room" button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,22 @@
+import { AnchorButton, Button, ButtonGroup } from "@blueprintjs/core";
+import { useCallback } from "react";
 import gridUnlockIcon from "../assets/grid-unlock.svg";
 
 export const Header = (): JSX.Element => {
+  const newRoom = useCallback(() => {
+    window.location.pathname = "/";
+  }, []);
+
   return (
     <div className="page-header">
       <h1>Grid Unlock</h1>
       <img src={gridUnlockIcon} className="grid-unlock-icon" alt="Grid Unlock Logo" />
+      <ButtonGroup className="buttons" minimal={true}>
+        <Button icon="grid-view" onClick={newRoom}>
+          New Room
+        </Button>
+        <AnchorButton rightIcon="caret-down"></AnchorButton>
+      </ButtonGroup>
     </div>
   );
 };
