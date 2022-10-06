a42c433a22a0f1b1c02e0e9281bdf280150c3753
David Skiff <dskiff@palantir.com>
2022-10-05 21:46:29 -0500

    halfway done

--- functions/api/puzzle/xword/[date].ts
+++ functions/api/puzzle/xword/[date].ts
@@ -9,6 +9,20 @@ interface PuzzleCacheEntry {
   puzzle: PuzzleDefinition;
 }
 
+// see: https://www.xwordinfo.com/JSON/
+interface XWordInfoJsonFormat {
+  title: string;
+  author: string;
+  editor: string;
+  copyright: string;
+  publisher: string;
+  date: string;
+  size: {
+    rows: number;
+    cols: number;
+  };
+}
+
 export const onRequestGet: PagesFunction<Env> = async ({ params, env }) => {
   try {
     const date = params.date as string;
@@ -21,6 +35,16 @@ export const onRequestGet: PagesFunction<Env> = async ({ params, env }) => {
     });
 
     const body = await req.text();
+    if (body == null || body.trim().length === 0) {
+      throw new Error("Somehow failed to get puzzle");
+    }
+
+    const parsed: XWordInfoJsonFormat = JSON.parse(body);
+
+    // Puzzles too far into the future have null values.
+    if (parsed.title == null) {
+      return new Response();
+    }
 
     return new Response(body);
   } catch (e) {
