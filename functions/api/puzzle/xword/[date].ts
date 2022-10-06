379559b487f3a090e549b58985025875a6709dfa
David Skiff <dskiff@palantir.com>
2022-10-05 20:04:00 -0500

    add a few more headers

--- functions/api/puzzle/xword/[date].ts
+++ functions/api/puzzle/xword/[date].ts
@@ -16,6 +16,7 @@ export const onRequestGet: PagesFunction<Env> = async ({ params, env }) => {
 
     const req = await fetch("https://www.xwordinfo.com/JSON/Data.ashx?format=text", {
       headers: {
+        "authority": "www.xwordinfo.com",
         "accept":
           "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
         "accept-language": "en-US,en;q=0.9",
@@ -29,6 +30,8 @@ export const onRequestGet: PagesFunction<Env> = async ({ params, env }) => {
         "sec-fetch-site": "same-origin",
         "sec-fetch-user": "?1",
         "upgrade-insecure-requests": "1",
+        "user-agent":
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36",
       },
     });
 
