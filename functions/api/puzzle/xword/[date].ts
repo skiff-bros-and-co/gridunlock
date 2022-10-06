b8236b4d8bccf60303f5234646c646ca9a6bf638
David Skiff <dskiff@palantir.com>
2022-10-06 14:51:58 -0500

    working?

--- functions/api/puzzle/xword/[date].ts
+++ functions/api/puzzle/xword/[date].ts
@@ -1,4 +1,5 @@
-import { PuzzleDefinition } from "../../../../src/state/Puzzle";
+import { Cell, Clue, PuzzleDefinition, PuzzleDirection, CellPosition } from "../../../../src/state/Puzzle";
+import { buildCellCluesByRowAndColumn } from "../../../../src/state/PuzzleDataBuilder";
 
 const TIMEOUTS_SEC = {
   UNAVAILABLE_KV: 1 * 60 * 60,
@@ -106,6 +107,10 @@ export async function fetchPuzzle(date: string, env: Env) {
     throw new Error("puzzle isn't yet available");
   }
 
+  const cells = generateCells(parsed);
+  const maxClueNumber = Math.max(
+    ...cells.flatMap((row) => row.map((cell) => cell.clueNumber)).filter((clueNumber) => clueNumber != null),
+  );
   const puzzle: PuzzleDefinition = {
     title: parsed.title,
     author: parsed.author,
@@ -113,13 +118,67 @@ export async function fetchPuzzle(date: string, env: Env) {
     description: parsed.notepad,
     height: parsed.size.rows,
     width: parsed.size.cols,
-    cells: [],
+    cells,
     clues: {
-      across: [],
-      byRowAndColumn: [],
-      clueCount: 0,
-      down: [],
+      across: parseClues(parsed.clues.across, "across", cells),
+      byRowAndColumn: buildCellCluesByRowAndColumn(cells),
+      clueCount: maxClueNumber,
+      down: parseClues(parsed.clues.down, "down", cells),
     },
   };
   return puzzle;
+}
+
+function generateCells(src: XWordInfoJsonFormat): Cell[][] {
+  const result: Cell[][] = [];
+
+  // grid is a flattened, row major array
+  for (let row = 0; src.size.rows; row++) {
+    const cells: Cell[] = [];
+    for (let col = 0; src.size.cols; col++) {
+      const index = row * src.size.cols + col;
+      const value = src.grid[index];
+      cells.push({
+        column: col,
+        row,
+        initialState: value === "." ? "." : "",
+        isBlocked: value === ".",
+        solution: value,
+        clueNumber: src.gridnums[index],
+      });
+    }
+    result.push(cells);
+  }
+
+  return result;
+}
+
+function parseClues(clueStrings: string[], direction: PuzzleDirection, cells: Cell[][]) {
+  const result: { [clueNumber: number]: Clue } = Object.create(null);
+
+  const positionLookup: { [clueNumber: number]: CellPosition } = Object.create(null);
+  for (let row = 0; cells.length; row++) {
+    for (let col = 0; cells[row].length; col++) {
+      const cell = cells[row][col];
+      if (cell.clueNumber != null) {
+        positionLookup[cell.clueNumber] = {
+          row: cell.row,
+          column: cell.column,
+        };
+      }
+    }
+  }
+
+  for (const clueString of clueStrings) {
+    const [clueNumberString, clue] = clueString.split(".", 2);
+    const clueNumber = Number(clueNumberString);
+    result[clueNumber] = {
+      clue,
+      clueNumber,
+      direction,
+      position: positionLookup[clueNumber],
+    };
+  }
+
+  return result;
 }
