b1f4c8bd4f7c056b0a788244e5bba389890792b2
David Skiff <dskiff@palantir.com>
2022-10-07 15:02:56 -0500

    do some more splitting

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig, splitVendorChunkPlugin } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
+import analyze from "rollup-plugin-analyzer";
+
+const NODE_MODULES_PATH = path.join(__dirname, "/node_modules/");
+const depPath = (module: string) => path.join(NODE_MODULES_PATH, module);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
-    alias: [{ find: /^~/, replacement: path.join(__dirname, "/node_modules/") }],
+    alias: [{ find: /^~/, replacement: NODE_MODULES_PATH }],
   },
-  plugins: [react(), splitVendorChunkPlugin()],
+  plugins: [react()],
   server: {
     proxy: {
       "/api": {
@@ -15,6 +19,36 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       },
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (!id.startsWith(NODE_MODULES_PATH)) {
+            return;
+          }
+
+          if (id.startsWith(depPath("y-")) || id.startsWith(depPath("simple-peer/"))) {
+            return "yjs";
+          }
+
+          if (id.startsWith(depPath("react/")) || id.startsWith(depPath("react-dom/"))) {
+            return "react";
+          }
+
+          if (id.startsWith(depPath("@blueprintjs/"))) {
+            return "blueprint";
+          }
+
+          return "vendor";
+        },
+      },
+      plugins: [
+        analyze({
+          limit: 10,
+        }),
+      ],
     },
   },
 });
