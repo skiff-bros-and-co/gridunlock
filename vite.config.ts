894c2652418b48299db4f1ca085949ebe7724481
David Skiff <dskiff@palantir.com>
2022-10-07 14:10:40 -0500

    try out split vender chunk

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
 
@@ -7,7 +7,7 @@ export default defineConfig({
   resolve: {
     alias: [{ find: /^~/, replacement: path.join(__dirname, "/node_modules/") }],
   },
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
   server: {
     proxy: {
       "/api": {
