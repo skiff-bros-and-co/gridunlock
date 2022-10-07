e728ec6acfee8642a95db66a0a49edfcbbef5184
David Skiff <dskiff@palantir.com>
2022-10-07 14:10:09 -0500

    point to new upstream api

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://gridunlock.pages.dev",
+        target: "https://gridunlock-org.pages.dev",
         changeOrigin: true,
         secure: false,
       },
