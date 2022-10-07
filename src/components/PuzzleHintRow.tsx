d2441c3efd2b98a55fe10a7afda39038ee931ecd
Ben Skiff <benskiff@msn.com>
2022-10-07 13:13:37 -0500

    Scroll the selected word hint into view

---
+++ src/components/PuzzleHintRow.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from "react";
+import { Clue } from "../state/Puzzle";
+
+interface Props {
+  clue: Clue;
+  isSelected: boolean;
+}
+
+export const PuzzleHintRow = (props: Props): JSX.Element => {
+  const ref = useRef<null | HTMLLIElement>(null);
+  useEffect(() => {
+    if (props.isSelected) {
+      ref.current?.scrollIntoView({ block: "nearest", inline: "nearest", behavior: "smooth" });
+    }
+  }, [props.isSelected, ref]);
+
+  return (
+    <li ref={ref} className={props.isSelected ? "selected-puzzle-hint" : ""}>
+      {props.clue.clueNumber}. {props.clue.clue}
+    </li>
+  );
+};
