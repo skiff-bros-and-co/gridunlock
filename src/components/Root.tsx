7a1f35dc420952e6d3b73aad383fd6ad82d8e112
David Skiff <dskiff@palantir.com>
2022-10-07 22:44:16 -0500

    reload page

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -19,7 +19,7 @@ export function Root() {
         action: {
           text: "Update",
           onClick() {
-            updateServiceWorker();
+            updateServiceWorker(true);
           },
         },
       });
