fd080108c1ae23da2dfb4ccbd8f8eb3b1092193b
David Skiff <dskiff@users.noreply.github.com>
2022-10-08 22:47:25 -0500

    add reference pictures

--- ete-tests/example.spec.ts
+++ ete-tests/example.spec.ts
@@ -1,5 +1,6 @@
-import { test } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 
 test("load page", async ({ page }) => {
   await page.goto("/");
+  await expect(page).toHaveScreenshot();
 });
--- playwright.config.ts
+++ playwright.config.ts
@@ -40,8 +40,6 @@ const config: PlaywrightTestConfig = {
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
-
-    screenshot: "on",
   },
 
   /* Configure projects for major browsers */
