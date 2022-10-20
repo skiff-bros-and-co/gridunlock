c65df71ec46281bf8133769d0d41f91dd5616e1f
David Skiff <dskiff@users.noreply.github.com>
2022-10-20 14:46:17 -0500

    add mobile media

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,8 +22,8 @@ function HeaderInternal(props: Props): JSX.Element {
 
   return (
     <div className="header">
-      <h1>Grid Unlock</h1>
-      <img src={gridUnlockIcon} className="grid-unlock-icon" alt="Grid Unlock Logo" />
+      <h1 className="app-title">Grid Unlock</h1>
+      <img src={gridUnlockIcon} className="app-icon" alt="Grid Unlock Logo" />
       <ButtonGroup className="buttons" minimal={true}>
         <Button icon="grid-view" onClick={newRoom}>
           New Room
