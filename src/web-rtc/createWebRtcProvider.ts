2e53a409a7716c3ed2110054596936d6769be4bc
David Skiff <dskiff@users.noreply.github.com>
2022-10-23 23:02:05 -0500

    try out xirsys

---
+++ src/web-rtc/createWebRtcProvider.ts
@@ -0,0 +1,38 @@
+import SimplePeer from "simple-peer";
+import { WebrtcProvider } from "y-webrtc";
+import * as Y from "yjs";
+import { ModifiedRTCPeerConnection } from "./ModifiedRTCPeerConnection";
+
+// This clearly provides no security other than mild obfustication.
+const PASSWORD = "princess_untitled_hurled-skydiver_clothes_hazily";
+
+type WebrtcProviderOptions = Partial<ConstructorParameters<typeof WebrtcProvider>[2]>;
+
+export function createWebRtcProvider(opts: {
+  roomName: string;
+  doc: Y.Doc;
+  iceServers: RTCIceServer[];
+}): WebrtcProvider {
+  const peerOpts: SimplePeer.Options = {
+    wrtc: {
+      RTCIceCandidate,
+      RTCSessionDescription,
+      RTCPeerConnection: ModifiedRTCPeerConnection,
+    },
+    config: {
+      iceServers: opts.iceServers,
+      iceTransportPolicy: "relay",
+    },
+  };
+
+  const webRtcOptions: WebrtcProviderOptions = {
+    password: PASSWORD,
+    peerOpts,
+    maxConns: 20 + Math.floor(Math.random() * 15),
+    filterBcConns: true,
+  };
+
+  // Types are bad
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return new WebrtcProvider(opts.roomName, opts.doc, webRtcOptions as any);
+}
