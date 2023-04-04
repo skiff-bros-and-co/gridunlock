76edb8bc9c507194048e2a4829053fedd4c6052f
David Skiff <dskiff@users.noreply.github.com>
2023-04-04 18:18:07 -0500

    split out intermediate test

--- test/parsers/parsePuz.test.ts
+++ test/parsers/parsePuz.test.ts
@@ -1,6 +1,5 @@
 import fs from "fs";
 import { describe, expect, test } from "vitest";
-import { parseIntermediatePuzzle } from "../../src/parsers/parseIntermediatePuzzle";
 import { parsePuz } from "../../src/parsers/parsePuz";
 
 /**
@@ -22,7 +21,7 @@ describe("parsePuz", () => {
 
   describe("when parsing a simple puzzle", () => {
     const simpleTestPuz = convertBufferToArrayBuffer(fs.readFileSync(__dirname + "/simple-generated.puz"));
-    const parsed = parseIntermediatePuzzle(parsePuz(simpleTestPuz));
+    const parsed = parsePuz(simpleTestPuz);
 
     test("should parse the title", () => {
       expect(parsed.title).toEqual("Simple Test");
@@ -48,156 +47,25 @@ describe("parsePuz", () => {
     test("should parse the clues", () => {
       expect(parsed.clues).toEqual({
         across: {
-          1: {
-            clue: "clue 1",
-            clueNumber: 1,
-            direction: "across",
-            position: {
-              row: 0,
-              column: 0,
-            },
-          },
-          "3": {
-            clue: "clue 3",
-            clueNumber: 3,
-            direction: "across",
-            position: {
-              column: 0,
-              row: 2,
-            },
-          },
+          1: "clue 1",
+          3: "clue 3",
         },
         down: {
-          2: {
-            clue: "clue 2",
-            clueNumber: 2,
-            direction: "down",
-            position: {
-              row: 0,
-              column: 1,
-            },
-          },
+          2: "clue 2",
         },
-        byRowAndColumn: [
-          [
-            {
-              isStartOfClue: true,
-              acrossClueNumber: 1,
-              downClueNumber: undefined,
-            },
-            {
-              isStartOfClue: true,
-              acrossClueNumber: 1,
-              downClueNumber: 2,
-            },
-            {
-              isStartOfClue: false,
-              acrossClueNumber: 1,
-              downClueNumber: undefined,
-            },
-          ],
-          [
-            undefined,
-            {
-              isStartOfClue: false,
-              acrossClueNumber: undefined,
-              downClueNumber: 2,
-            },
-            undefined,
-          ],
-          [
-            {
-              isStartOfClue: true,
-              acrossClueNumber: 3,
-              downClueNumber: undefined,
-            },
-            {
-              isStartOfClue: false,
-              acrossClueNumber: 3,
-              downClueNumber: 2,
-            },
-            {
-              isStartOfClue: false,
-              acrossClueNumber: 3,
-              downClueNumber: undefined,
-            },
-          ],
+        byCell: [
+          [1, 2, undefined],
+          [undefined, undefined, undefined],
+          [3, undefined, undefined],
         ],
-        clueCount: 3,
       });
     });
 
     test("should parse the cells", () => {
       expect(parsed.cells).toEqual([
-        [
-          {
-            column: 0,
-            row: 0,
-            solution: "C",
-            isBlocked: false,
-            clueNumber: 1,
-          },
-          {
-            column: 1,
-            row: 0,
-            solution: "A",
-            isBlocked: false,
-            clueNumber: 2,
-          },
-          {
-            column: 2,
-            row: 0,
-            solution: "T",
-            isBlocked: false,
-            clueNumber: undefined,
-          },
-        ],
-        [
-          {
-            column: 0,
-            row: 1,
-            solution: ".",
-            isBlocked: true,
-            clueNumber: undefined,
-          },
-          {
-            column: 1,
-            row: 1,
-            solution: "B",
-            isBlocked: false,
-            clueNumber: undefined,
-          },
-          {
-            column: 2,
-            row: 1,
-            solution: ".",
-            isBlocked: true,
-            clueNumber: undefined,
-          },
-        ],
-        [
-          {
-            column: 0,
-            row: 2,
-            solution: "E",
-            isBlocked: false,
-            clueNumber: 3,
-          },
-          {
-            column: 1,
-            row: 2,
-            solution: "C",
-            isBlocked: false,
-            clueNumber: undefined,
-          },
-          {
-            column: 2,
-            row: 2,
-            solution: "O",
-            isBlocked: false,
-            clueNumber: undefined,
-          },
-        ],
+        ["C", "A", "T"],
+        [".", "B", "."],
+        ["E", "C", "O"],
       ]);
     });
   });
--- test/parsers/parseXWord.test.ts
+++ test/parsers/parseXWord.test.ts
@@ -1,5 +1,4 @@
 import { describe, expect, test } from "vitest";
-import { parseIntermediatePuzzle } from "../../src/parsers/parseIntermediatePuzzle";
 import { XWordInfoJsonFormat, parseXWord } from "../../src/parsers/parseXWord";
 
 const TEST_XWORD: XWordInfoJsonFormat = {
@@ -22,7 +21,7 @@ const TEST_XWORD: XWordInfoJsonFormat = {
 
 describe("parseXWord", () => {
   describe("when parsing a simple puzzle", () => {
-    const puzzle = parseIntermediatePuzzle(parseXWord(TEST_XWORD));
+    const puzzle = parseXWord(TEST_XWORD);
 
     test("should parse the title", () => {
       expect(puzzle.title).toEqual("Test Puzzle");
@@ -48,156 +47,25 @@ describe("parseXWord", () => {
     test("should parse the clues", () => {
       expect(puzzle.clues).toEqual({
         across: {
-          1: {
-            clue: "Clue #1",
-            clueNumber: 1,
-            direction: "across",
-            position: {
-              row: 0,
-              column: 0,
-            },
-          },
-          3: {
-            clue: "Clue.With.Periods",
-            clueNumber: 3,
-            direction: "across",
-            position: {
-              column: 0,
-              row: 2,
-            },
-          },
+          1: "Clue #1",
+          3: "Clue.With.Periods",
         },
         down: {
-          2: {
-            clue: "Clue #2",
-            clueNumber: 2,
-            direction: "down",
-            position: {
-              row: 0,
-              column: 1,
-            },
-          },
+          2: "Clue #2",
         },
-        byRowAndColumn: [
-          [
-            {
-              isStartOfClue: true,
-              acrossClueNumber: 1,
-              downClueNumber: undefined,
-            },
-            {
-              isStartOfClue: true,
-              acrossClueNumber: 1,
-              downClueNumber: 2,
-            },
-            {
-              isStartOfClue: false,
-              acrossClueNumber: 1,
-              downClueNumber: undefined,
-            },
-          ],
-          [
-            undefined,
-            {
-              isStartOfClue: false,
-              acrossClueNumber: undefined,
-              downClueNumber: 2,
-            },
-            undefined,
-          ],
-          [
-            {
-              isStartOfClue: true,
-              acrossClueNumber: 3,
-              downClueNumber: undefined,
-            },
-            {
-              isStartOfClue: false,
-              acrossClueNumber: 3,
-              downClueNumber: 2,
-            },
-            {
-              isStartOfClue: false,
-              acrossClueNumber: 3,
-              downClueNumber: undefined,
-            },
-          ],
+        byCell: [
+          [1, 2, undefined],
+          [undefined, undefined, undefined],
+          [3, undefined, undefined],
         ],
-        clueCount: 3,
       });
     });
 
     test("should parse the cells", () => {
       expect(puzzle.cells).toEqual([
-        [
-          {
-            column: 0,
-            row: 0,
-            solution: "C",
-            isBlocked: false,
-            clueNumber: 1,
-          },
-          {
-            column: 1,
-            row: 0,
-            solution: "A",
-            isBlocked: false,
-            clueNumber: 2,
-          },
-          {
-            column: 2,
-            row: 0,
-            solution: "T",
-            isBlocked: false,
-            clueNumber: undefined,
-          },
-        ],
-        [
-          {
-            column: 0,
-            row: 1,
-            solution: ".",
-            isBlocked: true,
-            clueNumber: undefined,
-          },
-          {
-            column: 1,
-            row: 1,
-            solution: "REBUS",
-            isBlocked: false,
-            clueNumber: undefined,
-          },
-          {
-            column: 2,
-            row: 1,
-            solution: ".",
-            isBlocked: true,
-            clueNumber: undefined,
-          },
-        ],
-        [
-          {
-            column: 0,
-            row: 2,
-            solution: "E",
-            isBlocked: false,
-            clueNumber: 3,
-          },
-          {
-            column: 1,
-            row: 2,
-            solution: "C",
-            isBlocked: false,
-            clueNumber: undefined,
-          },
-          {
-            column: 2,
-            row: 2,
-            solution: "O",
-            isBlocked: false,
-            clueNumber: undefined,
-          },
-        ],
+        ["C", "A", "T"],
+        [".", "REBUS", "."],
+        ["E", "C", "O"],
       ]);
     });
   });
