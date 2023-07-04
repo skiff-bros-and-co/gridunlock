47d1d1b083c62fb5907bb7cb2995c74a8ad0188f
David Skiff <dskiff@users.noreply.github.com>
2023-07-04 18:22:28 -0400

    respond to ping

--- functions/api/rtc/signaling.ts
+++ functions/api/rtc/signaling.ts
@@ -1,6 +1,25 @@
+interface YWebRtcSubscriptionMessage {
+  type: "subscribe" | "unsubscribe";
+  topics?: string[];
+}
+interface YWebRtcPingMessage {
+  type: "ping";
+}
+
+interface YWebRtcPongMessage {
+  type: "pong";
+}
+interface YWebRtcPublishMessage {
+  type: "publish";
+  topic?: string;
+  [k: string]: unknown;
+}
+
 interface Env {}
 
 export const onRequest: PagesFunction<Env> = async ({ request }) => {
+  console.log("request", JSON.stringify(request.headers, null, 2));
+
   const upgradeHeader = request.headers.get("Upgrade");
   if (!upgradeHeader || upgradeHeader !== "websocket") {
     return new Response("Expected Upgrade: websocket", { status: 426 });
@@ -11,8 +30,22 @@ export const onRequest: PagesFunction<Env> = async ({ request }) => {
 
   server.accept();
   server.addEventListener("message", (event) => {
-    console.log(event.data);
-    server.send("Hello from the server!");
+    const message: YWebRtcSubscriptionMessage | YWebRtcPingMessage | YWebRtcPublishMessage | { type: unknown } =
+      JSON.parse(event.data as string);
+
+    switch (message.type) {
+      case "subscribe":
+      case "unsubscribe":
+      case "ping": {
+        const msg: YWebRtcPongMessage = { type: "pong" };
+        server.send(JSON.stringify(msg));
+        break;
+      }
+      case "publish":
+        break;
+      default:
+        console.log("unknown message type", message.type);
+    }
   });
 
   return new Response(null, {
