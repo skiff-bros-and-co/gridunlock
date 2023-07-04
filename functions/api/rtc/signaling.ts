98fa1f43c4627420856b1eb4a3d95dfed784fd23
David Skiff <dskiff@users.noreply.github.com>
2023-07-04 18:27:44 -0400

    accept subscriptions

--- functions/api/rtc/signaling.ts
+++ functions/api/rtc/signaling.ts
@@ -28,23 +28,45 @@ export const onRequest: PagesFunction<Env> = async ({ request }) => {
   const webSocketPair = new WebSocketPair();
   const [client, server] = Object.values(webSocketPair);
 
+  let channel: string | undefined;
+
   server.accept();
   server.addEventListener("message", (event) => {
-    const message: YWebRtcSubscriptionMessage | YWebRtcPingMessage | YWebRtcPublishMessage | { type: unknown } =
-      JSON.parse(event.data as string);
+    const message: YWebRtcSubscriptionMessage | YWebRtcPingMessage | YWebRtcPublishMessage = JSON.parse(
+      event.data as string,
+    );
 
     switch (message.type) {
-      case "subscribe":
-      case "unsubscribe":
+      case "subscribe": {
+        if (message.topics?.length !== 1 || channel != null) {
+          console.log("expected 1 topic");
+        } else {
+          channel = message.topics?.[0];
+        }
+        break;
+      }
+      case "unsubscribe": {
+        if (message.topics?.length !== 1 || channel !== message.topics?.[0]) {
+          console.log("expected 1 topic");
+        } else {
+          channel = undefined;
+        }
+        break;
+      }
       case "ping": {
         const msg: YWebRtcPongMessage = { type: "pong" };
         server.send(JSON.stringify(msg));
         break;
       }
       case "publish":
+        if (channel == null) {
+          console.log("no channel");
+        } else {
+          console.log("publish", message);
+        }
         break;
       default:
-        console.log("unknown message type", message.type);
+        console.log("unknown message type", message);
     }
   });
 
